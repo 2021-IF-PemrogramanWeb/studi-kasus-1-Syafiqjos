3c348e58923deba4ebe92e59e35efc5bcadb1ef6
Syafiqjos <syafiqjos@gmail.com>
2021-10-28 15:08:30 +0700

    Add tryRegister method in UserController

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -36,6 +36,45 @@
         );
     }
 
+    function tryRegister($db, $email, $password, $password_conf) {
+        if ($res = $db->query("select * from users where email = \"$email\";")) {
+            if ($res->num_rows == 1) {
+                // email exist then error
+                $row = $res->fetch_row();
+                return array(
+                    "status" => 1,
+                    "data" => null
+                );
+            } else {
+                if ($password == $password_conf) {
+                    // password conf same
+                    $res = $db->query("INSERT into users (email, password) values ('$email', '$password');");
+                    return array(
+                        "status" => 0,
+                        "data" => null
+                    );
+                } else {
+                    return array(
+                        "status" => 2,
+                        "data" => null
+                    );
+                }
+            }
+
+            $res->free_result();
+            
+            return array(
+                "status" => 1,
+                "data" => null
+            );
+        }
+
+        return array(
+            "status" => -1,
+            "data" => null
+        );
+    }
+
     function logout($db) {
         setcookie('email', null);
         setcookie('pass', null);
