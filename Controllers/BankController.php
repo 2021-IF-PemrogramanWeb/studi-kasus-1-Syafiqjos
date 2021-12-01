f5dc58fbf201e9e69d6584a9f15cdba87c8aa985
Syafiqjos <syafiqjos@gmail.com>
2021-12-02 06:06:21 +0700

    Update sql string escape from bank data user input on BankController

--- Controllers/BankController.php
+++ Controllers/BankController.php
@@ -1,6 +1,7 @@
 <?php
 
     function getBankData($db, $userId) {
+        $userId = $db->real_escape_string($userId);
         if ($res = $db->query("select * from banks where user_id = $userId order by date;")) {
             $data = array();
             while($row = mysqli_fetch_assoc($res)) {
@@ -14,15 +15,17 @@
     }
 
     function addBankData($db, $user_id, $data){
-        $desc = $data['description'];
-        $val = $data['value'];
-        $date = $data['date'];
+        $user_id = $db->real_escape_string($user_id);
+        $desc = $db->real_escape_string($data['description']);
+        $val = $db->real_escape_string($data['value']);
+        $date = $db->real_escape_string($data['date']);
 
-        $res = $db->query("INSERT INTO banks(user_id, description, value, date) VALUES ('$user_id','$desc','$val','$date')");
+        $res = $db->query("INSERT INTO banks(user_id, description, value, date) VALUES ('$user_id','$desc','$val','$date');");
     }
 
     function deleteBankData($db, $user_id, $data) {
-        $id = $data['id'];
+        $user_id = $db->real_escape_string($user_id);
+        $id = $db->real_escape_string($data['id']);
 
         $res = $db->query("DELETE FROM banks WHERE user_id = $user_id AND id = $id;");
     }
